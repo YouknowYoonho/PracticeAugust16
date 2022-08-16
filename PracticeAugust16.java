2742444027e3fd4bd758b113d271b83fcd0ceb3b
YouknowYoonho <111064772+YouknowYoonho@users.noreply.github.com>
2022-08-16 09:02:12 -0400

    Update PracticeAugust16.java

--- PracticeAugust16.java
+++ PracticeAugust16.java
@@ -4,7 +4,7 @@ public class PracticeAugust16
   public static void main(String [] args)
   {
      System.out.println("Dr. Hyesung Park"); 
-     //Katie Nguyen
+     //Katie Nguyen //Yoonho Shin
     
     
     
